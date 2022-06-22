a21bf65adcdf01bc08a31c6c9db0ae60373c45d3
Daniel Moya <dnmoyha@gmail.com>
2022-06-22 16:24:37 -0400

    Cambios de la sección de programas y proyectos, pero falta gran parte...

--- js/app.js
+++ js/app.js
@@ -75,3 +75,6 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 	return new bootstrap.Tooltip(tooltipTriggerEl);
 });
+
+/* Gallery Programs and Projects */
+
